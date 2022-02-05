be850daccc0a8c157d29cc7e8c1d93748908c677
Matthew Pull <thematthewpull@gmail.com>
2022-02-05 19:24:45 +0000

    Avoid including Tailwind classes in git

--- new-iamstuck/src/index.js
+++ new-iamstuck/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './index.css';
+import './index_compiled.css';
 import reportWebVitals from './reportWebVitals';
 import MentorDashboard from './mentorDashboard';
 
