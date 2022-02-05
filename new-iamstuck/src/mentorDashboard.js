184d68ec6e5f421125e1cb7b87a0ef7a7f47643b
Hao Liang <hl5617@ic.ac.uk>
2022-02-05 23:20:03 +0000

    Move request list into requestList.js

--- new-iamstuck/src/mentorDashboard.js
+++ new-iamstuck/src/mentorDashboard.js
@@ -2,17 +2,17 @@
 import {Fragment} from 'react'
 import {Disclosure, Menu, Transition} from '@headlessui/react'
 import {BellIcon, MenuIcon, XIcon} from '@heroicons/react/outline'
-import {MailIcon, PhoneIcon} from '@heroicons/react/solid'
+import RequestsList from "./requestList";
 
 const people = [
     {
-        name: 'What is 3x+4?',
-        title: 'Jane Cooper',
+        name: 'How to solve 3x+4=2?',
+        title: 'Tom Nook',
         role: 'Maths',
-        email: 'janecooper@example.com',
+        email: 'tomnook@example.com',
         telephone: '+1-202-555-0170',
         imageUrl:
-            'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60',
+            'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
     },
     {
         name: 'How long is a piece of string?',
@@ -205,62 +205,17 @@ export default function MentorDashboard() {
 
                 <header className="bg-white shadow">
                     <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        <h1 className="text-3xl font-bold text-gray-900">Dashboard</h1>
+                        <h1 className="text-3xl font-bold text-gray-900">Helper Dashboard</h1>
                     </div>
                 </header>
                 <main>
                     <div className="max-w-3xl mx-auto py-6 sm:px-6 lg:px-8">
                         {/* Start replace */}
-                        {RequestsList()}
+                        {RequestsList(people)}
                         {/* /End replace */}
                     </div>
                 </main>
             </div>
         </>
-    )
-}
-
-function RequestsList() {
-    return (
-        <ul className="grid grid-cols-1 gap-6 ">
-            {people.map((person) => (
-                <li key={person.email} className="col-span-1 bg-white rounded-lg shadow divide-y divide-gray-200">
-                    <div className="w-full flex items-center justify-between p-6 space-x-6">
-                        <div className="flex-1 truncate">
-                            <div className="flex items-center space-x-3">
-                                <h3 className="text-gray-900 text-sm font-medium truncate">{person.name}</h3>
-                                <span className="flex-shrink-0 inline-block px-2 py-0.5 text-green-800 text-xs font-medium bg-green-100 rounded-full">
-                                  {person.role}
-                                </span>
-                            </div>
-                            <p className="mt-1 text-gray-500 text-sm truncate">{person.title}</p>
-                        </div>
-                        <img className="w-10 h-10 bg-gray-300 rounded-full flex-shrink-0" src={person.imageUrl} alt=""/>
-                    </div>
-                    <div>
-                        <div className="-mt-px flex divide-x divide-gray-200">
-                            <div className="w-0 flex-1 flex">
-                                <a
-                                    href={`mailto:${person.email}`}
-                                    className="relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500"
-                                >
-                                    <MailIcon className="w-5 h-5 text-gray-400" aria-hidden="true"/>
-                                    <span className="ml-3">Dismiss</span>
-                                </a>
-                            </div>
-                            <div className="-ml-px w-0 flex-1 flex">
-                                <a
-                                    href={`tel:${person.telephone}`}
-                                    className="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500"
-                                >
-                                    <PhoneIcon className="w-5 h-5 text-gray-400" aria-hidden="true"/>
-                                    <span className="ml-3">Accept</span>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </li>
-            ))}
-        </ul>
     )
 }
