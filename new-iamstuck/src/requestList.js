19ec5a285965c09ace26ca7c06cdc6b846374295
Hao Liang <hl5617@ic.ac.uk>
2022-02-06 01:53:36 +0000

    Remove button link on request list

--- new-iamstuck/src/requestList.js
+++ new-iamstuck/src/requestList.js
@@ -27,7 +27,7 @@ export default function RequestsList(people, buttons=true) {
                             <div className="-mt-px flex divide-x divide-gray-200">
                                 <div className="w-0 flex-1 flex">
                                     <a
-                                        href='/'
+                                        href='#'
                                         className="relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500"
                                     >
                                         <XIcon className="w-5 h-5 text-gray-400" aria-hidden="true"/>
@@ -36,7 +36,7 @@ export default function RequestsList(people, buttons=true) {
                                 </div>
                                 <div className="-ml-px w-0 flex-1 flex">
                                     <a
-                                        href="/call"
+                                        href="#"
                                         className="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500"
                                     >
                                         <CheckIcon className="w-5 h-5 text-gray-400" aria-hidden="true"/>
