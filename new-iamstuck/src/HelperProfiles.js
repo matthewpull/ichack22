11f478ba301e95874802285cc2fe68776054e6fc
Hao Liang <hl5617@ic.ac.uk>
2022-02-06 05:44:56 +0000

    Connect helper dashboard to backened

--- new-iamstuck/src/HelperProfiles.js
+++ new-iamstuck/src/HelperProfiles.js
@@ -30,7 +30,7 @@ export default function HelperProfiles() {
         return <div>Loading...</div>;
     } else {
         return (
-            <ul role="list" className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3">
+            <ul className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3">
                 {items.map((person) => (
                     <li
                         key={person.id}
--- new-iamstuck/src/requestList.js
+++ new-iamstuck/src/requestList.js
@@ -1,26 +1,55 @@
 import {XIcon, CheckIcon} from '@heroicons/react/solid'
+import {useEffect, useState} from "react";
 
 export default function RequestsList(people, buttons=true) {
-    return (
-        <ul className="grid grid-cols-1 gap-6 ">
-            {people.map((person) => (
-                <li key={person.email} className="col-span-1 bg-white rounded-lg shadow divide-y divide-gray-200">
-                    <div className="w-full flex items-center justify-between p-6 space-x-6">
-                        <div className="flex-1 truncate">
-                            <div className="flex items-center space-x-3">
-                                <h3 className="text-textdark text-sm font-medium truncate">{person.name}</h3>
-                                <span className="flex-shrink-0 inline-block px-2 py-0.5 text-green-800 text-xs font-medium bg-green-100 rounded-full">
-                                  {person.role}
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://${process.env.REACT_APP_BASE_URL}/questions/unanswered/`)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setItems(result);
+                },
+                // Note: it's important to handle errors here
+                // instead of a catch() block so that we don't swallow
+                // exceptions from actual bugs in components.
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+    }, [])
+    if (error) {
+        return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+        return <div>Loading...</div>;
+    } else {
+        console.log(items)
+        return (
+            <ul className="grid grid-cols-1 gap-6 ">
+                {items.map((item) => (
+                    <li key={item.id} className="col-span-1 bg-white rounded-lg shadow divide-y divide-gray-200">
+                        <div className="w-full flex items-center justify-between p-6 space-x-6">
+                            <div className="flex-1 truncate">
+                                <div className="flex items-center space-x-3">
+                                    <h3 className="text-textdark text-sm font-medium truncate">{item.title}</h3>
+                                    <span
+                                        className="flex-shrink-0 inline-block px-2 py-0.5 text-green-800 text-xs font-medium bg-green-100 rounded-full">
+                                  {item.level}
                                 </span>
-                                <span className="flex-shrink-0 inline-block px-2 py-0.5 text-red-800 text-xs font-medium bg-red-100 rounded-full">
-                                  {person.role}
+                                    <span
+                                        className="flex-shrink-0 inline-block px-2 py-0.5 text-red-800 text-xs font-medium bg-red-100 rounded-full">
+                                  {item.topic}
                                 </span>
+                                </div>
+                                <p className="mt-1 text-gray-500 text-sm truncate">{item.text}</p>
                             </div>
-                            <p className="mt-1 text-gray-500 text-sm truncate">{person.title}</p>
                         </div>
-                        <img className="w-10 h-10 bg-gray-300 rounded-full flex-shrink-0" src={person.imageUrl} alt=""/>
-                    </div>
-                    {buttons &&
+                        {buttons &&
                         <div>
                             <div className="flex divide-x divide-gray-200">
                                 <div className="w-0 flex-1 flex">
@@ -42,9 +71,10 @@ export default function RequestsList(people, buttons=true) {
 
                             </div>
                         </div>
-                    }
-                </li>
-            ))}
-        </ul>
-    )
+                        }
+                    </li>
+                ))}
+            </ul>
+        )
+    }
 }
